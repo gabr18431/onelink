3369750f778217b8f7dab61c9561b5679bf9de86
Gaber Ahmed <145846033+gabr18431@users.noreply.github.com>
2024-06-18 15:50:26 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,38 @@ window.onscroll = function() {
         document.querySelector('header').classList.remove('sticky');
     }
 }
+
+
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    });
+
+// handel Show Content 
+let allTabs = document.querySelectorAll('.connections .tabs button');
+function handelShowContent(btn) {
+    allTabs.forEach(b => {
+        if (b.title === btn.title) {
+            b.classList.add('active');
+        } else {
+            b.classList.remove('active');
+        }
+    })
+    if (btn.title === 'short-link') {
+        document.querySelector('.landing-page-content').style.display = 'none';
+        document.querySelector('.QR-code-content').style.display = 'none';
+        document.querySelector('.short-link-content').style.display = 'block';
+    } else if (btn.title === 'QR-code') {
+        document.querySelector('.short-link-content').style.display = 'none';
+        document.querySelector('.landing-page-content').style.display = 'none';
+        document.querySelector('.QR-code-content').style.display = 'block';
+    } else {
+        document.querySelector('.short-link-content').style.display = 'none';
+        document.querySelector('.QR-code-content').style.display = 'none';
+        document.querySelector('.landing-page-content').style.display = 'block';
+    }
+}
